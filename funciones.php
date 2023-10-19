522b89927a97cd4a95538ba86a42dda653abe0ee
dastarache <128723319+dastarache@users.noreply.github.com>
2023-10-19 08:04:04 -0500

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -3,14 +3,15 @@
         $salida=0;//inicializa la varible 
          $conexion=mysqli_connect('localhost','root','root','nueva' );//crea conexion a la base de datos 
          $sql="select 2+1";
-               $sql.="as suma";// concatena con el sql de arriba para ahorrar espacio
+               $sql.=" as suma";// concatena con el sql de arriba para ahorrar espacio
                $r=$conexion->query($sql);//ejecuta una consulta
-         return $salida;// retorna la operacion 
+         
          //recorre el recordset
-         while($fila=mysqli_fetch_assoc($sr)){
+         while($fila=mysqli_fetch_assoc($r)){
             // inch o acumula 
             $salida+=$fila['suma'];
          }
+         return $salida;// retorna la operacion 
 
          }
 
