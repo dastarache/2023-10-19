c22a7191c2d0cbd299962f8d26f2a4b6f50f71cb
dastarache <128723319+dastarache@users.noreply.github.com>
2023-10-19 07:31:45 -0500

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -1,7 +1,12 @@
 <?php 
-echo "hola";
-
-
-
-
+    function consulta(){
+        $salida=0;//inicializa la varible 
+        $salida=10*2/2;//calcula el area del triangulo
+    return $salida;// retorna la operacion 
+}
+   function jugar(){
+        $salida=0;//inicializa la varible 
+        $salida=45*3/2;//calcula el area del triangulo
+    return $salida;// retorna la operacion 
+   }
 ?>
