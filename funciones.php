821be0e13d5c1e18c37931d50938cbb7df104a8e
dastarache <128723319+dastarache@users.noreply.github.com>
2023-10-19 07:50:59 -0500

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -3,8 +3,8 @@
         $salida=0;//inicializa la varible 
          $conexion=mysqli_connect('localhost','root','root','nueva' );//crea conexion a la base de datos 
          $sql="select 2+1";
-               $sql.="as suma";
-               $r=$conexion->query($sql);
+               $sql.="as suma";// concatena con el sql de arriba para ahorrar espacio
+               $r=$conexion->query($sql);//ejecuta una consulta
          return $salida;// retorna la operacion 
 }
 ?>
